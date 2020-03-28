b2a56625449aaab895dc6ecae55f0d0d4e98a1d8
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-03-28 15:47:37 -0300

    Validações e testes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,18 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './src/database/dev.sqlite3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite3'
     },
     migrations: {
       directory: './src/database/migrations'
